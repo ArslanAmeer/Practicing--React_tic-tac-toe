f487b375be4c172fd32e2b738058d159ce8b2d1c
Arslan Ameer <arslan.ameer@hotmail.com>
2024-08-26 16:46:27 +0500

    ♻️ Update: Disable already selected box

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -5,16 +5,14 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard({ onSelectSquare, turns }) {
-
   let gameBoard = initialGameBoard;
 
   for (const turn of turns) {
-    const {square, player} = turn;
-    const {row, col} = square;
+    const { square, player } = turn;
+    const { row, col } = square;
 
     gameBoard[row][col] = player;
   }
-
 
   // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
@@ -35,7 +33,10 @@ export default function GameBoard({ onSelectSquare, turns }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
                   {playerSymbol}
                 </button>
               </li>
